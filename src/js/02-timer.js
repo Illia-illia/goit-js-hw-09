c2aa262b39ed953b15bc313b746a4061598badee
Illia-Illia <Takoexerrorx@gmail.com>
2022-08-19 17:48:47 +0300

    task 2 20 procent

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,19 +1,58 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const fpicker = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('[data-start]');
+const nowDate = Date.now();
 const fp = flatpickr(fpicker, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    onChoseTime(selectedDates[0].getTime());
   },
 });
 
-const select = fp.selectedDates;
-const nowDate = new Date();
+btnStart.setAttribute('disabled', 'true');
 
-console.log(new Date());
-console.log(fp.selectedDates);
+function onChoseTime(setDate) {
+  const diference = setDate - nowDate;
+  if (diference > 0) {
+    btnStart.removeAttribute('disabled');
+    return console.log(convertMs(diference));
+  }
+  Notify.failure('Please choose a date in the future');
+  btnStart.setAttribute('disabled', 'true');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// const timer1 = {
+//   start() {
+//     const dataStart = Date.now();
+//     setInterval(() => {
+//       const currentTime = Date.now();
+//       console.log(currentTime + dataStart);
+//     }, 1000);
+//   },
+// };
+// timer1.start();
