a016ce25f9770434cc46434bf5b0a0077a6f6bdb
Illia-Illia <Takoexerrorx@gmail.com>
2022-08-20 12:46:21 +0300

    finish task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,34 +1,72 @@
+// 1 Якщо користувач вибрав дату в минулому, покажи window.alert() з текстом "Please choose a date in the future".
+// 2 Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
+// 3 Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому.
+// 4 Натисканням на кнопку «Start» починається відлік часу до обраної дати з моменту натискання.
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const fpicker = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('[data-start]');
-const nowDate = Date.now();
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let userDate = 0;
 const fp = flatpickr(fpicker, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    onChoseTime(selectedDates[0].getTime());
+    onChosenTime(selectedDates[0].getTime());
   },
 });
 
 btnStart.setAttribute('disabled', 'true');
 
-function onChoseTime(setDate) {
-  const diference = setDate - nowDate;
+function onChosenTime(setDate) {
+  const nowDate = Date.now();
+  const difference = setDate - nowDate;
 
-  if (diference > 0) {
+  if (difference > 0) {
     btnStart.removeAttribute('disabled');
-
-    return console.log(convertMs(diference));
+    return (userDate = setDate);
   }
   Notify.failure('Please choose a date in the future');
   btnStart.setAttribute('disabled', 'true');
 }
 
+btnStart.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+  btnStart.setAttribute('disabled', 'true');
+  fpicker.setAttribute('disabled', 'true');
+  const nowDate = Date.now();
+  const difference = userDate - nowDate;
+  onTimer(difference);
+  onTimer(difference);
+}
+
+function onTimer(value) {
+  const dataStart = Date.now();
+  const intervalId = setInterval(() => {
+    const currentTime = Date.now();
+    const difference = currentTime - dataStart;
+    let result = value - difference;
+    onSetInterface(convertMs(result));
+    if (result <= 1000) {
+      fpicker.removeAttribute('disabled');
+      return clearInterval(intervalId);
+    }
+  }, 1000);
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -37,24 +75,20 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = pad(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = pad(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
 
-// const timer1 = {
-//   start() {
-//     const dataStart = Date.now();
-//     setInterval(() => {
-//       const currentTime = Date.now();
-//       console.log(dataStart - currentTime);
-//     }, 1000);
-//   },
-// };
-// timer1.start();
+function onSetInterface({ days, hours, minutes, seconds }) {
+  secondsEl.textContent = seconds;
+  minutesEl.textContent = minutes;
+  hoursEl.textContent = hours;
+  daysEl.textContent = days;
+}
