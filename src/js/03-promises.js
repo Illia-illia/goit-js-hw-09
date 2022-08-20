8c0c18eab9f4eb0ba30562aee0551f5eee700aad
Illia-Illia <Takoexerrorx@gmail.com>
2022-08-20 15:02:55 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,7 +7,7 @@
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-refs = {
+const refs = {
   delayEl: document.querySelector('input[name="delay"]'),
   stepEl: document.querySelector('input[name="step"]'),
   amountEl: document.querySelector('input[name="amount"]'),
