492f30998e2e607d2f5fc6bdb7acdfcc46a4f76b
Illia-Illia <Takoexerrorx@gmail.com>
2022-08-20 14:56:31 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,29 @@
-bodyBg = document.querySelector('body');
-startBtn = document.querySelector('button[data-start]');
-stopBtn = document.querySelector('button[data-stop]');
+refs = {
+  bodyBg: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
-startBtn.addEventListener('click', onStartClick);
-stopBtn.addEventListener('click', onStopClick);
-stopBtn.setAttribute('disabled', 'true');
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+refs.stopBtn.setAttribute('disabled', 'true');
 
 let idColorChanger = null;
 
 function onStartClick() {
-  startBtn.setAttribute('disabled', 'true');
-  stopBtn.removeAttribute('disabled');
+  refs.startBtn.setAttribute('disabled', 'true');
+  refs.stopBtn.removeAttribute('disabled');
   startChangeBodyColor();
 }
 function onStopClick() {
-  startBtn.removeAttribute('disabled');
-  stopBtn.setAttribute('disabled', 'true');
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', 'true');
   stopChangeBodyColor();
 }
 
 function startChangeBodyColor() {
   idColorChanger = setInterval(() => {
-    bodyBg.style.backgroundColor = getRandomHexColor();
+    refs.bodyBg.style.backgroundColor = getRandomHexColor();
   }, 1000);
 }
 
